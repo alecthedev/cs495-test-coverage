0ef64744b68de4294109ec89d102c6a2c3a7cdb3
Alec Burch-DeWitt <168141387+alecthedev@users.noreply.github.com>
2025-11-16 17:02:48 +0000

    Add tests to achieve 100% coverage

--- AtmServices.Test/AtmTests.cs
+++ AtmServices.Test/AtmTests.cs
@@ -19,4 +19,27 @@ public class AtmTests
         Assert.True(result);
         Assert.Equal(75,testAtm.getBalance());
     }
+
+    [Fact]
+    public void Test_Withdraw_Overdraw()
+    {
+        var result = testAtm.withdraw(110);
+        Assert.False(result);
+    }
+
+        [Fact]
+    public void Test_Deposit()
+    {
+        var result = testAtm.deposit(10);
+        Assert.True(result);
+        Assert.Equal(110,testAtm.getBalance());
+    }
+
+        [Fact]
+    public void Test_Deposit_Negative()
+    {
+        var result = testAtm.deposit(-10);
+        Assert.False(result);
+    }
+
 }
